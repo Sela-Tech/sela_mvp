8cb807bf2886c6ed6b14f18b3af4b27b07d497da
mhkane <mhkane@mit.edu>
2017-10-04 13:45:07 -0400

    Node server with mongodb running

---
+++ sela_app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-demo");
+var nameSchema = new mongoose.Schema({
+    projectName: String,
+    villageName: String
+});
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/splash", (req, res) => {
+    res.sendFile(__dirname + "/splash.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Project saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
