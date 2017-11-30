675438d0764bda2cd3dbc0975c5eba4a0dd0b88c
konoufo <konoufo1@gmail.com>
2017-11-30 17:26:57 -0500

    fetch port in env first

--- sela_app.js
+++ sela_app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var dotenv = require('dotenv');
 var path = require('path')
