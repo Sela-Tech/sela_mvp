8912ec5088e770800d08eebdb0431fdc24ceebe4
Bisike Nnadi <bisike@hyrecar.com>
2018-01-08 08:59:19 -0800

    adds user variable

--- app/controllers/project_controller.js
+++ app/controllers/project_controller.js
@@ -1,5 +1,6 @@
 //libraries
 var mongoose = require('mongoose');
+
 // classes
 var Controller = require('./base_controller');
 
@@ -8,14 +9,24 @@ var controller = new Controller();
 var UserModel = mongoose.model('User');
 var ProjectModel = mongoose.model('Project');
 
-controller.create = function(req, res, next) {};
+controller.create = function(req, res, next) {
+    var user = req.user || {};
+};
 
-controller.read = function(req, res, next) {};
+controller.read = function(req, res, next) {
+    var user = req.user || {};
+};
 
-controller.readAll = function(req, res, next) {};
+controller.readAll = function(req, res, next) {
+    var user = req.user || {};
+};
 
-controller.update = function(req, res, next) {};
+controller.update = function(req, res, next) {
+    var user = req.user || {};
+};
 
-controller.delete = function(req, res, next) {};
+controller.delete = function(req, res, next) {
+    var user = req.user || {};
+};
 
 module.exports = controller;
