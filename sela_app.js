21fa986910be1009458292ab13ef69c5e9951d68
Bisike Nnadi <bisike@hyrecar.com>
2018-01-09 08:12:52 -0800

    merge fix

--- sela_app.js
+++ sela_app.js
@@ -45,7 +45,6 @@ environmentsAll.call(app);
 // });
 
 
-<<<<<<< HEAD
 // app.get("/project", (req, res) => {
 //     res.sendFile(__dirname + "/public/project_creation/material_project.html");
 // });
@@ -68,20 +67,3 @@ server.listen(process.env.PORT, () => {
 });
 
 routes.call(app);
-=======
-app.post("/project_post", (req, res) => {
-    console.log('Post Request received '+req.body.projectName);
-    // Create a Project object with data filled from the form
-    var project = new Project(
-        { project_name: req.body.projectName,
-          project_description: req.body.projectDescription
-        });
-    project.save(function (err) {
-         if (err) return handleError(err);
-    console.log('New Project: ' + project);      });
-    });
-
-app.listen(port, () => {
-    console.log("Server listening on port " + port);
-});
->>>>>>> master
