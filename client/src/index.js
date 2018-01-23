32f668adfd201fd05d01ae37046cf17b971a27b0
konoufo <konoufo1@gmail.com>
2018-01-23 05:15:15 -0500

    redux basic structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/index.css';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import App from './App';
+import './assets/css/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(rootReducer);
+
+const Root = ({store}) => (
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 registerServiceWorker();
