d5d62743ca0a0b7021a905f2c716a2b332bafa13
mhkane <mhkane@mit.edu>
2018-02-17 18:47:44 -0500

    Register helper

--- sela_bot/controllers/register_user.js
+++ sela_bot/controllers/register_user.js
@@ -1,7 +1,20 @@
 'use strict'
 
 const Telegram=require('telegram-node-bot');
+var dotenv = require('dotenv');
+var mongoose = require('mongoose');
 
+
+dotenv.config();
+var mongoDB = process.env.MONGOLAB_URI;
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// Compiling models
+var User = mongoose.model('users');
 class RegisterController extends Telegram.TelegramBaseController {
     /**
      * @param {Scope} $
@@ -18,6 +31,14 @@ class RegisterController extends Telegram.TelegramBaseController {
         else {
             var username = query_terms[1] 
             $.sendMessage('You are attempting to register user ' + username + ' and the chat id is: '+chat_id)
+            User.find({ 'user_name': username },'first_name family_name')
+                .exec(function (err) {
+                    if (err) {
+                        console.log('No user with that name');
+                    }
+                    console.log('success');
+                });
+
         }
         
     }
