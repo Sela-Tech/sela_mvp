03b2b7d1d59cd170d974fd366ed9c548efac7d72
kezike17 <kezike17@mit.edu>
2018-04-16 17:05:13 -0400

    Configured login and signup endpoints

--- sela_app.js
+++ sela_app.js
@@ -4,13 +4,14 @@ var port = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var dotenv = require('dotenv');
 var path = require('path')
-
-
+var MongoClient = require('mongodb').MongoClient;
+var MongoURI = process.env.MONGO_URI;
+var MongoDbName = "sela_dev";
+var MongoUsersName = "users";
 
 dotenv.config();
 
 app.use(express.static(__dirname + '/public/'));
-
 
 /*var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
@@ -23,6 +24,50 @@ var User = mongoose.model("User", nameSchema);*/
 
 app.get("/", (req, res) => {
     res.redirect("/index");
+});
+
+app.get("/login?uname=:uname&pass=:pass", (req, resAuth) => {
+    var authSuccess = false;
+    MongoClient.connect(MongoURI, (connErr, db) => {
+        if (connErr) throw connErr;
+        var selaDb = db.db(MongoDbName);
+        var authQuery = {};
+        authQuery.username = req.params.uname;
+        authQuery.password = req.params.pass;
+        selaDb.collection(MongoUsersName).find(authQuery).toArray((authErr, subAuthRes) => {
+            if (authErr) throw authErr;
+            for (var i = 0; i < res.length; i++) {
+              if (subAuthRes.username == authQuery.username) {
+                authSuccess = subAuthRes.password == authQuery.password;
+              }
+            }
+            db.close();
+        });
+    });
+    resAuth.send(authSuccess);
+});
+
+app.post("/signup?uname=:uname&pubkey=:pubkey&pass=:pass", (req, resReg) => {
+    var regSuccess = true;
+    MongoClient.connect(MongoURI, (connErr, db) => {
+        if (connErr) throw connErr;
+        var selaDb = db.db(MongoDbName);
+        var regQuery = {};
+        regQuery.username = req.params.uname;
+        regQuery.pubkey = req.params.pubkey;
+        query.password = req.params.pass;
+        selaDb.collection(MongoUsersName).find(regQuery).toArray((regErr, subRegRes) => {
+            if (regErr) throw regErr;
+            for (var i = 0; i < res.length; i++) {
+              if (subRegRes.username == regQuery.username) {
+                regSuccess = false;
+                break;
+              }
+            }
+            db.close();
+        });
+    });
+    resReg.send(regSuccess);
 });
 
 app.get("/project", (req, res) => {
