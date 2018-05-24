a897dc31f6096649361e3fd089efd503b6b3d03c
kezike17 <kezike17@mit.edu>
2018-05-24 13:21:07 -0400

    Investigating project post error

--- route_tests/postProject.sh
+++ route_tests/postProject.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -v -H "Content-Type: application/x-www-form-urlencoded" -d @sample_post_project_data.json -X POST https://sela-labs.herokuapp.com/project
+curl -v -H "Content-Type: application/json" -d @sample_post_project_data.json -X POST https://sela-labs.herokuapp.com/project
