53c9449298928ae2e86545ea408f4eb49c0e66d6
Dotun <dotunlonge@Dotuns-MBP>
2018-05-31 12:24:01 +0100

    made centering styling globally available

--- src/pages/errors/404.js
+++ src/pages/errors/404.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-const Error404 = ()=> {
-    return <div>
+const Error404 = () => {
+  return (
+    <div className="center-wrapper">
+      <div className="center">
         <p>This is a 404</p>
+      </div>
     </div>
-  
-}
+  );
+};
 
 export default Error404;
