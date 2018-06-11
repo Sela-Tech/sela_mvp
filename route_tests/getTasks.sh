9f21bb5acd309bcccfff955a3b07a21f4e8eae1d
kezike17 <kezike17@mit.edu>
2018-06-11 14:08:01 -0400

    Using 'x-access-token' as opposed to 'X-Access-Token'

--- route_tests/getTasks.sh
+++ route_tests/getTasks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -v -H "Content-Type: application/json" -H "X-Access-Token: " -d @sample_get_tasks_data.json -X GET https://sela-labs.herokuapp.com/tasks
+curl -v -H "Content-Type: application/json" -H "X-Access-Token: $1" -d @sample_get_tasks_data.json -X GET https://sela-labs.herokuapp.com/tasks
