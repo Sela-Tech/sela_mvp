e2666ab766d152efe7abf9011c11941d78b38d1c
Dotun <dotunlonge@Dotuns-MBP>
2018-06-26 17:30:26 +0100

    prod setup 2

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,5 +1,4 @@
-const b = process.env.REACT_APP_BACKEND_SERVER_BASEURL,
-  t = process.env.mREACT_APP_BACKEND_SERVER_BASEURL;
+const b = process.env.REACT_APP_BACKEND_SERVER_BASEURL;
 
 export default {
   signin: b + "login",
@@ -7,9 +6,9 @@ export default {
   send_recovery_mail: "",
   verify_user_token: b + "verifyToken",
 
-  fetch_projects: t + "projects",
-  fetch_project: t + "project/",
-  add_project: t + "projects",
-  add_task: t + "task",
-  fetch_taks: t + "projects" //:id/tasks
+  fetch_projects: b + "projects",
+  fetch_project: b + "project/",
+  add_project: b + "projects",
+  add_task: b + "task",
+  fetch_taks: b + "projects" //:id/tasks
 };
