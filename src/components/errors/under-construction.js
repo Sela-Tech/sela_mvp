ec17e90e5b5e5ea9a45d90011f666e9f11d51b53
Dotun <dotunlonge@Dotuns-MBP>
2018-06-28 16:38:32 +0100

    sign-out straight from under construction page

--- src/components/errors/under-construction.js
+++ src/components/errors/under-construction.js
@@ -1,7 +1,8 @@
 import React from "react";
-import history from "../../helpers/history";
+import { signout } from "../../store/action-creators/auth";
+import { connect } from "react-redux";
 
-const UnderConstruction = () => {
+const UnderConstruction = ({ dispatch }) => {
   return (
     <p
       className="xs-10 xs-off-1 sm-6 sm-off-3 md-4 md-off-4"
@@ -13,16 +14,18 @@ const UnderConstruction = () => {
       still under construction or us developers are still fighting the good
       battle with the codebase. Come back later. :)
       <button
-        onClick={() => history.goBack()}
+        onClick={() => {
+          dispatch(signout());
+        }}
         style={{
           display: "block",
-          margin: "auto"
+          margin: "2em auto"
         }}
       >
-        Go Back
+        Fine, I'll Log Out
       </button>
     </p>
   );
 };
 
-export default UnderConstruction;
+export default connect()(UnderConstruction);
