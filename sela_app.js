169953f9be7830462145f6f54f0fdcdc2cd5051e
Dotun <dotunlonge@Dotuns-MBP>
2018-06-28 13:35:56 +0100

    added token

--- sela_app.js
+++ sela_app.js
@@ -94,7 +94,7 @@ app.post("/verifyToken", (req, res) => {
         return res.status(500).json(failRes);
       }
       req.userId = user._id;
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+      res.setHeader(`Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, ${tokenHeaderField}`);
       return res.status(200).json(successRes);
     });
 });
