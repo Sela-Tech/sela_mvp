b284436dc2726e352bb6179916a0300ee4bf18c2
kezike17 <kezike17@mit.edu>
2018-07-18 03:27:08 -0400

    Created model for Transaction

---
+++ app/models/transaction.js
@@ -0,0 +1,140 @@
+var moment = require('moment');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var transactionStructure = {
+    id: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    sender: {
+        type: ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiver: {
+        type: ObjectId,
+        ref: 'User',
+        required: true
+    },
+    value: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['PENDING', 'CONFIRMED'],
+        default: "PENDING" 
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedOn: {
+        type: Date,
+        default: Date.now()
+    }
+};
+
+var schemaOptions = {
+    collection: 'transactions',
+    minimize: false,
+    id: false,
+    toJSON: {
+        getters: true,
+        virtuals: true,
+        minimize: false,
+        versionKey: false,
+        retainKeyOrder: true
+    },
+    toObject: {
+        getters: true,
+        virtuals: true,
+        minimize: false,
+        versionKey: false,
+        retainKeyOrder: true
+    },
+    autoIndex: process.env.NODE_ENV === 'development',
+    strict: process.env.NODE_ENV !== 'development'
+};
+
+if (process.env.NODE_ENV === 'development') {
+    transactionStructure.test = {
+        type: Boolean,
+        default: true,
+    };
+}
+
+var locationStructure = {
+    name: {
+        type: String,
+        required: true
+    },
+    lat: {
+        type: Number,
+        required: true
+    },
+    long: {
+        type: Number,
+        required: true
+    }
+};
+
+var schemaOptions = {
+    minimize: false,
+    id: false,
+    toJSON: {
+        getters: true,
+        virtuals: true,
+        minimize: false,
+        versionKey: false,
+        retainKeyOrder: true
+    },
+    toObject: {
+        getters: true,
+        virtuals: true,
+        minimize: false,
+        versionKey: false,
+        retainKeyOrder: true
+    },
+    autoIndex: process.env.NODE_ENV === 'development',
+    strict: process.env.NODE_ENV !== 'development'
+};
+
+var locationSchema = new Schema(locationStructure, schemaOptions);
+
+var TransactionSchema = new Schema(transactionStructure, schemaOptions);
+
+TransactionSchema.pre('save', true, function(next, done) {
+
+    next();
+
+    this.updatedOn = new Date();
+
+    done();
+});
+
+TransactionSchema.pre('update', true, function(next, done) {
+
+    next();
+
+    this.update({}, {
+        $set: {
+            updatedOn: new Date()
+        }
+    });
+
+    done();
+});
+
+//Export model
+/*module.exports = function(connection) {
+
+    if (!connection) {
+        connection = mongoose;
+    }
+    connection.model('Transaction', TransactionSchema);
+};*/
+module.exports = mongoose.model('Transaction', TransactionSchema);
