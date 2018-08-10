447d95c0424c5035d7cedf7aaa4b5bbc920e433b
Dotun <dotunlonge@Dotuns-MBP>
2018-08-10 13:01:10 +0100

    Modified footer to reflect recent redesign

--- src/components/home/footer.js
+++ src/components/home/footer.js
@@ -1,14 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Instagram from "../../assets/icons/Instagram.svg";
+import Instagram from "../../assets/icons/instagram.svg";
 import Youtube from "../../assets/icons/youtube.svg";
 import Telegram from "../../assets/icons/telegram.svg";
-// import LogoWhite from "../../assets/icons/logo-white.svg";
+import Twitter from "../../assets/icons/twitter.svg";
+import Medium from "../../assets/icons/medium.svg";
+import LogoWhite from "../../assets/icons/logo-white.svg";
 
 const Footer = styled.div`
   padding: 2em 0;
-  background: #f9fafc;
+  background: #07121f;
 
   #copyright {
     text-align: center;
@@ -21,18 +23,36 @@ const Footer = styled.div`
   p,
   a {
     font-weight: 300;
+    display: inline-block;
+  }
+
+  .soc-media {
+    line-height: 5em;
   }
   .social-media {
-    height: 4em;
-    width: 4em;
-    padding: 0 0.25em;
+    height: 2em;
+    width: 2em;
+    margin: 0 0.8em;
     display: inline-block;
     object-fit: contain;
     object-position: center;
   }
 
   #copyright {
-    margin-top: 6em;
+    margin-top: 1.25em;
+    color: #828282;
+    display: block;
+  }
+
+  nav {
+    a {
+      color: white;
+      padding: 1em;
+      line-height: 2em;
+      @media (max-width: 767px) {
+        display: block;
+      }
+    }
   }
 `;
 
@@ -40,51 +60,43 @@ export default () => {
   return (
     <Footer className="xs-12">
       <div className="xs-10 xs-off-1">
-        <div className="xs-12 sm-4">
+        <div className="xs-12 md-2">
           <Link to="/">
             <span id="logo">
-              {/* <img src={LogoWhite} alt="logo-white" /> */}
+              <img src={LogoWhite} alt="logo-white" />
             </span>
           </Link>
-          <p> Building communities of transparency in the emerging world </p>
         </div>
 
-        <div className="xs-12 sm-4">
-          {/* <h4>Quick Links</h4>
-          <ul>
-            <li>
-              <Link to="/join-telegram">Join Telegram Group</Link>
-            </li>
-            <li>
-              <Link to="/contact-us">Contact Us</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/careers">Careers</Link>
-            </li>
-          </ul> */}
+        <div className="xs-12 md-6">
+          <nav>
+            <Link to="/join-telegram">Projects</Link>
+            <Link to="/contact-us">Blog</Link>
+            <Link to="/about">Privacy Policy</Link>
+            <Link to="/blog">Terms of Use</Link>
+          </nav>
         </div>
 
-        <div className="xs-12 sm-4">
-          <h4> Connect with us </h4>
-          <a href="instagram" target="_blank">
-            <img src={Instagram} className="social-media" alt="instagram" />
-          </a>
+        <div className="xs-12 md-4 soc-media">
           <a href="youtube" target="_blank">
             <img src={Youtube} alt="youtube" className="social-media" />
           </a>
           <a href="telegram" target="_blank">
             <img src={Telegram} alt="telegram" className="social-media" />
           </a>
+          <a href="twitter" target="_blank">
+            <img src={Twitter} alt="twitter" className="social-media" />
+          </a>
+          <a href="instagram" target="_blank">
+            <img src={Instagram} className="social-media" alt="instagram" />
+          </a>
+          <a href="medium" target="_blank">
+            <img src={Medium} alt="medium" className="social-media" />
+          </a>
         </div>
       </div>
       <div className="xs-12">
-        <p id="copyright">Copyright (c) 2018. Sela Labs</p>
+        <p id="copyright"> &copy; Sela 2018</p>
       </div>
     </Footer>
   );
