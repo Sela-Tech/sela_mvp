fde751b7efb3cfc4a624a79069390d0ac76a7671
Dotun <dotunlonge@Dotuns-MBP>
2018-09-03 12:27:21 +0100

    Made single spinner component

--- src/components/spinners/index.js
+++ src/components/spinners/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const Div = styled.div`
+const DivOne = styled.div`
   .lds-ellipsis {
     display: inline-block;
     position: relative;
@@ -59,13 +59,73 @@ const Div = styled.div`
   }
 `;
 
-export default () => (
-  <Div>
+const DivTwo = styled.div`
+  .lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 0.85em;
+    height: 0.85em;
+  }
+  .lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 1em;
+    height: 1em;
+    border: 3px solid #fff;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #fff transparent transparent transparent;
+  }
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+const Two = () => (
+  <DivTwo>
+    <div className="lds-ring">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  </DivTwo>
+);
+
+const One = () => (
+  <DivOne>
     <div className="lds-ellipsis">
       <div />
       <div />
       <div />
       <div />
     </div>
-  </Div>
+  </DivOne>
 );
+
+export default ({ type }) => {
+  switch (type) {
+    case "one":
+      return <One />;
+
+    case "two":
+      return <Two />;
+
+    default:
+      return null;
+  }
+};
