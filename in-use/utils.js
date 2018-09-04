f3ad1140b8d0cd3ea3368745a51251d0a76a361d
Dotun <dotunlonge@Dotuns-MBP>
2018-09-04 17:30:57 +0100

    Verify token utility

---
+++ in-use/utils.js
@@ -0,0 +1,47 @@
+var visibilityHeaderField = "public";
+var tokenHeaderField = "x-access-token";
+
+var jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+  const whitelisted = ["/projects"];
+
+  const token = req.headers[tokenHeaderField],
+    public = req.headers[visibilityHeaderField];
+
+  if (typeof token === "undefined" && public) {
+    // check if the route is whitelisted
+
+    let isWhitelisted = Boolean(
+      whitelisted.filter(url => {
+        return req.path === url;
+      }).length
+    );
+
+    req.token = false;
+
+    if (isWhitelisted) {
+      next();
+    } else {
+      res.status(400).json({
+        message: "No Token Provided"
+      });
+    }
+  } else if (typeof token !== "undefined") {
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+      if (err) {
+        return res.status(400).json({
+          message: err.message
+        });
+      } else {
+        req.userId = decoded.id;
+        req.decodedTokenData = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({
+      message: "No Token Provided"
+    });
+  }
+};
