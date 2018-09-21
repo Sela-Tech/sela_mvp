0eda767e2bb5703a3e817dd99253f6820758ce33
mhkane <mhkane@mit.edu>
2018-09-21 01:45:00 -0700

    Image upload route via multer

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@
 var { verifyToken } = require("../in-use/utils");
 let user_controller = require("../app/controllers/user");
 
+const multer = require('multer');
+const upload = multer({dest:'uploads/'});
+
 module.exports = function(app) {
   //real routes
   app.route("/register").post(user_controller.register);
@@ -21,4 +24,8 @@ module.exports = function(app) {
   app.route("/password").post(verifyToken, user_controller.change_password);
 
   app.route("/verifyToken").post(verifyToken, user_controller.verify);
+  app.route("/imageUpload")
+    .post(upload.single('verificationImage'), (req,res, next) => {
+      console.log(req.file);
+  });
 };
