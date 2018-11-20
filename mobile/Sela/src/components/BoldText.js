c24dc1cc993e0c705fec5c062e8a4d0114bd1fed
linux08 <abimbola130@gmail.com>
2018-11-20 23:27:37 +0100

    Added completed password screen

--- mobile/Sela/src/components/BoldText.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
-
-
-const B = (props) => <Text style={{ fontWeight: 'bold' }}>{props.children}</Text>
-export default B;
--- mobile/Sela/src/utils/constants.js
+++
@@ -1,1 +0,0 @@
-export const DEFAULT_COLOUR = '#201D41';
