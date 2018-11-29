57ad166995a70bb838f081c1c36ca8bd5cc55872
mhkane <mohamedkane@Mohameds-MacBook-Pro.local>
2018-11-29 19:09:37 +0100

    Site upload along with picture of site

--- old/sela_bot/bot_upload_site.py
+++ old/sela_bot/bot_upload_site.py
@@ -27,15 +27,26 @@ from dotenv import load_dotenv
 import telegram
 import pymongo
 import datetime
-
+import boto3
+import boto
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+   
 
 
 # Accessing variables.
+
+
 dotenv_path = join(dirname(__file__), '.env')
 print(dotenv_path)
 load_dotenv(dotenv_path)
 telegram_token = os.getenv('SELA_BOT_API')
 print(telegram_token)
+
+
+
+evidence_name = ''
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -52,7 +63,13 @@ Lat, Long = 0,0
 
 lat_long = db['site_registration']
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+PHOTO, LOCATION, SITE_REG = range(3)
+
+post = {
+            "latitude": Lat,
+            "longitude": Long,
+            "evidence_name": evidence_name
+}
 
 
 
@@ -60,27 +77,33 @@ GENDER, PHOTO, LOCATION, BIO = range(4)
 def start(bot, update):
     user = update.message.from_user
     first_name = user.first_name
-    Location_message = 'Hello ' + str(first_name) + ' Would you please like to send me your location'
+    Location_message = 'Hello ' + str(first_name) + ' Would you please like to send me a picture'
     update.message.reply_text(Location_message)
-    return LOCATION
+    return PHOTO
 
-
-def location(bot, update):
+def photo(bot, update):
     user = update.message.from_user
-    user_location = update.message.location
-    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
-                user_location.longitude)
-    Lat, Long = user_location.latitude, user_location.longitude
+    photo_file = bot.get_file(update.message.photo[-1].file_id)
+    evidence_name = user.first_name + '_' + user.last_name + '_' + str(datetime.datetime.utcnow()) + '.jpg'
+    post['evidence_name'] = evidence_name
+    post['author'] = str(user.first_name)
+    post['datetime'] = str(datetime.datetime.utcnow())
+    photo_file.download(evidence_name)
+    aws_access = os.getenv('AWSaccessKeyId')
+    aws_secret = os.getenv('AWSsecretAccessKey')
+    conn = S3Connection(str(aws_access), str(aws_secret))
+    bucket_name = os.getenv('BUCKET_UPLOAD')
+    bucket = conn.create_bucket(bucket_name,
+        location=boto.s3.connection.Location.DEFAULT)
+    k = Key(bucket)
+    testfile = evidence_name
+    k.key = evidence_name
+    k.set_contents_from_filename(testfile)    
+    logger.info("Photo of %s: %s", user.first_name, evidence_name)
+    update.message.reply_text('Gorgeous! Now, send me your location please, '
+                              'or send /skip if you don\'t want to.')
 
-    post = {"author": user.first_name,
-            "latitude": Lat,
-            "longitude": Long,
-            "date": datetime.datetime.utcnow()
-    }
-    lat_long.insert_one(post)
-
-    update.message.reply_text('Thank you for sending your location, what is the name of the site ?')
-    return BIO
+    return LOCATION
 
 
 def skip_location(bot, update):
@@ -89,13 +112,38 @@ def skip_location(bot, update):
     update.message.reply_text('You seem a bit paranoid! '
                               'At last, tell me something about yourself.')
 
-    return BIO
+    return SITE_REG
 
 
-def bio(bot, update):
+def location(bot, update):
     user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    user_location = update.message.location
+    Lat, Long = user_location.latitude, user_location.longitude
+    post['latitude'] = user_location.latitude
+    post['longitude'] = user_location.longitude
+
+    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
+                user_location.longitude)
+    update.message.reply_text('Thank you for sending your location, what is the name of the site ?')
+    return SITE_REG
+
+
+
+
+def skip_photo(bot, update):
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    update.message.reply_text('I bet you look great! Now, send me your location please, '
+                              'or send /skip.')
+
+    return LOCATION
+
+
+def site_reg(bot, update):
+    user = update.message.from_user
+    logger.info("Site of %s: %s", user.first_name, update.message.text)
     update.message.reply_text('Thank you! I hope we can talk again some day.')
+    lat_long.insert_one(post)
 
     return ConversationHandler.END
 
@@ -121,16 +169,18 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    # Add conversation handler with the states PHOTO, LOCATION and SITE_REG
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
 
         states={
-
             LOCATION: [MessageHandler(Filters.location, location),
                        CommandHandler('skip', skip_location)],
 
-            BIO: [MessageHandler(Filters.text, bio)]
+            PHOTO: [MessageHandler(Filters.photo, photo),
+                    CommandHandler('skip', skip_photo)],
+
+            SITE_REG: [MessageHandler(Filters.text, site_reg)],
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
