c4594182b70c2e453bec8ac6dde41673d8fe67f3
Dotun <oludotunlonge@gmail.com>
2019-01-31 14:28:23 +0100

    created single actions file for modal actions

---
+++ app/src/store/actions/modals.js
@@ -0,0 +1,13 @@
+export default {
+    add_task: "Add Task",
+    add_project: "Propose A Project",
+    view_task: "View Task",
+    view_image: "View Image",
+    delete_project: "Delete Project",
+    add_stakeholder: "Add Stakeholder",
+    add_transaction: "Add Transaction",
+    add_document: "Add Document",
+    view_stakeholder: "Sela Citizen Card",
+    set_interests: "set_interests"
+  };
+  
