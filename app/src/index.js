82669bdbd3c38dc5c253e8b742dd12743130eada
Dotun <oludotunlonge@gmail.com>
2019-02-18 23:14:07 +0100

    should stop looping

--- app/src/index.js
+++ app/src/index.js
@@ -1,39 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
  import App from "./App";
-// import registerServiceWorker from './registerServiceWorker';
+//  import registerServiceWorker from './registerServiceWorker';
+ import { unregister } from './registerServiceWorker';
+ 
 import store from "./store";
-import { Provider } from "react-redux";
+import Provider from "react-redux/lib/components/Provider";
 import { verify_user_token } from "./store/action-creators/auth";
 import { retrieveToken } from "./helpers/TokenManager";
 import { get_notifications, store_socket_data } from "./store/action-creators/notifications";
-import { fetchSGDs } from "./store/action-creators/app";
 import io from 'socket.io-client';
 import ends from "./endpoints";
 import notifications_actions from "./store/actions/notifications";
-
-let dispatch_count = 0;
+import ToastContainer from 'react-toastify/lib/components/ToastContainer';
 
 if (retrieveToken()) {
   store.dispatch(verify_user_token());
 }
-
   const socket = io(ends.b);
   let loop_stop = false;
- 
   socket.on("connected",function(data){
     console.log("socket connected");
 
     store.subscribe(()=>{
+      // reload web pages if error boundary is reset
+      if(store.getState().app.errorBoundaryKey > 0){
+        setTimeout(()=>{
+          window.location.reload();
+        },500)
+      }
 
       let retrieved_token = Boolean(retrieveToken());
-    
-      if ( retrieved_token === true && loop_stop === false ){  
-        console.log('here')
-        loop_stop = true;
+      if ( retrieved_token === true && loop_stop === false && store.getState().auth.isAuthenticated === true){  
         
+        loop_stop = true;
         store.dispatch(get_notifications());
-         
+
         let obj = {
           "userId": store.getState().auth.credentials.id,// the authenticated userId
           "socketId": data.user  // the socketId received   
@@ -45,7 +47,6 @@ if (retrieveToken()) {
         };
 
         store.dispatch(store_socket_data(data));
-        
       }
 
       let isAuthenticated = store.getState().auth.isAuthenticated;
@@ -62,12 +63,10 @@ if (retrieveToken()) {
     });  
 
   socket.on("notifications", function(data){
-
     let { notifications, unreadNIds }  = data.notifications;
-   
     if( typeof(notifications) === "object"){
       store.dispatch({
-        type: notifications_actions.UPDATE_NOTIFICATIONS_SUCCESSFUL,
+        type: notifications_actions.UPDATE_NOTIFICATIONS_S,
         notifications,
         unreadNIds
       })
@@ -75,14 +74,15 @@ if (retrieveToken()) {
 
   });
 
-
- store.dispatch(fetchSGDs());
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+   <React.Fragment>
+      <ToastContainer/>
+      <Provider store={store}>
+          <App />
+      </Provider>
+    </React.Fragment>,
   document.getElementById("root")
 );
 
-// registerServiceWorker();
+//  registerServiceWorker();
+unregister();
