d5c1bde5e6321882bf11528d85b97cb71da5f71a
Dotun Longe <oludotunlonge@gmail.com>
2019-04-08 16:47:46 +0100

    documents on the public description for a project

--- src/pages/public/projects/view-project/subs/description/index.js
+++ src/pages/public/projects/view-project/subs/description/index.js
@@ -114,8 +114,55 @@ const DescriptionWrapper = styled.div`
     }
   }
 
+
+  .document{
+    .inner{
+        margin-bottom: 7px;
+        border-top: 1px solid #eee;  
+    }
+    .text{
+        display: block;
+        float: left;
+        padding-top: 6px;
+
+        h4,p{
+            margin:0;
+        }
+        h4{
+            font-weight: 300;
+            line-height: 20px;
+            font-size: 14px;
+            color: #222829;
+        }
+        p{
+            font-size: 10px;
+            color: #696F74;
+            font-weight: 200;
+        }
+    }
+    .download{
+            display: block;
+            height: 100%;
+            float: right;
+            padding-top: 15px;
+        a{
+            font-size: 10px;
+            font-weight: 300;
+            text-align: right;
+            color: #696F74;
+            display: block;
+
+            &:hover{
+                color: green ;
+            }
+        }
+    }
+}
+
 `;
 export default ({ id, project }) => {
+  console.log(project)
+
   return (
     <DescriptionWrapper className="xs-12">
       <div className="xs-10 xs-off-1">
@@ -163,6 +210,33 @@ export default ({ id, project }) => {
                 }): null: null  
             }
             </div>
+
+            <div className='xs-12 add-doc'>
+              <h5>Additional documents</h5>
+              
+              <div className='xs-12 sm-10 document'>
+                  {
+                      project.documents && project.documents.map((doc,i)=>{
+                      let type = doc.filetype.split("/")[0];
+                      if(type !== 'image' && type !== 'video' && type !== 'audio'){
+                          type = 'document';
+                      }
+                      return <div className='xs-12 inner' key={i}>
+                          <div className={`img preview-${type} xs-3 sm-2`}/>
+                          <div className='text xs-6 sm-7'>
+                          <h4>{doc.name}</h4>
+                              <p>{doc.size}</p>
+                          </div>
+                          <div className='download xs-2 sm-2'>
+                              <a href={doc.doc} target="_blank" rel="noopener noreferrer"> Download</a>
+                          </div>
+                      </div>
+                      })
+                      
+                  }
+              </div>
+
+          </div>
 
           </div>
         </div>
